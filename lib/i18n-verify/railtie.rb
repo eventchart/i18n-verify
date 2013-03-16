fbf758b2a6f500d1a92659e49fdf399f6b92898e
Stefan Wienert <stwienert@gmail.com>
2013-03-16 13:02:18 +0100

    included i18n tasks from other libraries

--- lib/i18n-verify/railtie.rb
+++ lib/i18n-verify/railtie.rb
@@ -2,6 +2,8 @@ module I18nVerify
   class I18nVerifyRailtie < Rails::Railtie
     rake_tasks do
       load "tasks/verify.rake"
+      load "tasks/missing.rake"
+      load "tasks/translate.rake"
     end
   end
 end
